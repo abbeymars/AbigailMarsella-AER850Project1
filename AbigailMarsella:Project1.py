e8d4f00df70d1330ac31084805de1675d880e6e2
abbeymars <abigail.marsella@torontomu.ca>
2024-10-19 16:01:57 -0400

    Update 3 models

--- AbigailMarsella:Project1.py
+++ AbigailMarsella:Project1.py
@@ -10,7 +10,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
 from sklearn.model_selection import GridSearchCV
@@ -57,45 +56,39 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2,random
 
 #Linear Regression Model
 linear_regression = LinearRegression()
-parametric_grid_lr = {}
+parametric_grid_lr = {
+    }
 grid_search_lr = GridSearchCV(linear_regression, parametric_grid_lr, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
 grid_search_lr.fit(X_train, y_train)
 best_model_lr = grid_search_lr.best_estimator_
-Y_pred_log = grid_search_lr.predict(X_test)
-accuracy_log = accuracy_score(y_test, Y_pred_log)
+Y_pred_lr = grid_search_lr.predict(X_test)
+accuracy_lr = accuracy_score(y_test, Y_pred_lr)
 print("Best Linear Regression Model:", best_model_lr)
-print(f"Linear Regression Test Accuracy: {accuracy_log:.2f}")
-
+print(f"Linear Regression Test Accuracy: {accuracy_lr}")
 
 #Random Forest Classifier (with RandomizedSearchCV)
-CV_model = RandomForestClassifier()
-grid_search = RandomizedSearchCV(CV_model, param_distributions=param_grid, cv=5, scoring='neg_mean_absolute_error', n_jobs=1)
-grid_search.fit(X_train, Y_train)
-Y_pred_CV = grid_search.predict(X_test)
-
-best_params = grid_search.best_params_
-print("Best Hyperparameters:", best_params)
-best_modelCV = grid_search.best_estimator_
-
-
-accuracy_CV = accuracy_score(Y_test, Y_pred_CV)
-print(f"CV Accuracy: {accuracy_CV}")
-
-
-
-
+random_forest = RandomForestClassifier()
+parametric_grid_rf = {
+    }
+grid_search_rf = RandomizedSearchCV(random_forest, param_distributions=parametric_grid_rf, cv=5, scoring='neg_mean_absolute_error', n_jobs=1)
+grid_search_rf.fit(X_train, y_train)
+best_model_rf = grid_search_rf.best_estimator_
+Y_pred_rf = grid_search_rf.predict(X_test)
+accuracy_rf = accuracy_score(y_test, Y_pred_rf)
+print("Best Random Forest Model:", best_model_rf)
+print(f"Random Forest Test Accuracy: {accuracy_rf}")
 
 #Support Vector Machine
-svr = SVR()
-param_grid_svr = {
-    'kernel': ['linear', 'rbf'],
-    'C': [0.1, 1, 10, 100],
-    'gamma': ['scale', 'auto']
-}
-grid_search_svr = GridSearchCV(svr, param_grid_svr, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
-grid_search_svr.fit(X_train, y_train)
-best_model_svr = grid_search_svr.best_estimator_
-print("Best SVM Model:", best_model_svr)
+svm = SVR()
+parametric_grid_svm = {
+    }
+grid_search_svm = GridSearchCV(svm, parametric_grid_svm, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
+grid_search_svm.fit(X_train, y_train)
+best_model_svm = grid_search_svm.best_estimator_
+Y_pred_svm = grid_search_svm.predict(X_test)
+accuracy_svm = accuracy_score(y_test, Y_pred_svm)
+print("Best Support Vector Machine Model:", best_model_svm)
+print(f"Support Vector Machine Accuracy: {accuracy_rf}")
 
 
 
