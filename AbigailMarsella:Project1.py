dc797566db03d1893ef5861a2b65f47f180eea86
abbeymars <abigail.marsella@torontomu.ca>
2024-10-19 18:10:21 -0400

    Update Step 4 and 5

--- AbigailMarsella:Project1.py
+++ AbigailMarsella:Project1.py
@@ -16,7 +16,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import accuracy_score
-
+from sklearn.metrics import precision_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
 
@@ -66,8 +66,8 @@ grid_search_lr.fit(X_train, y_train)
 best_model_lr = grid_search_lr.best_estimator_
 Y_pred_lr = grid_search_lr.predict(X_test)
 accuracy_lr = accuracy_score(y_test, Y_pred_lr)
+precision_lr = precision_score(y_test, Y_pred_lr, average='weighted')
 print("Best Logistic Regression Model:", best_model_lr)
-print(f"Logistic Regression Test Accuracy: {accuracy_lr}")
 
 #Random Forest Classifier (with RandomizedSearchCV)
 random_forest = RandomForestClassifier()
@@ -80,8 +80,8 @@ grid_search_rf.fit(X_train, y_train)
 best_model_rf = grid_search_rf.best_estimator_
 Y_pred_rf = grid_search_rf.predict(X_test)
 accuracy_rf = accuracy_score(y_test, Y_pred_rf)
+precision_rf = precision_score(y_test, Y_pred_rf, average='weighted')
 print("Best Random Forest Model:", best_model_rf)
-print(f"Random Forest Test Accuracy: {accuracy_rf}")
 
 #Support Vector Machine
 svm = SVC()
@@ -94,13 +94,43 @@ grid_search_svm.fit(X_train, y_train)
 best_model_svm = grid_search_svm.best_estimator_
 Y_pred_svm = grid_search_svm.predict(X_test)
 accuracy_svm = accuracy_score(y_test, Y_pred_svm)
+precision_svm = precision_score(y_test, Y_pred_svm, average='weighted')
 print("Best Support Vector Machine Model:", best_model_svm)
-print(f"Support Vector Machine Accuracy: {accuracy_svm}")
 
 
 #Step 5 - Model Performance Analysis
-#Compare the overall performance of each model based on f1 score, precision and accuracy.
-#Based on the selected model, create a confusion matrix to visualize the performance of your model
+#F1 Scores
+f1_lr = f1_score(y_test, Y_pred_lr, average='weighted')
+f1_rf = f1_score(y_test, Y_pred_rf, average='weighted')
+f1_svm = f1_score(y_test, Y_pred_svm, average='weighted')
+
+#Choose best Model based on F1 Score, Precision and Accuracy
+print(f"Logistic Regression F1 Score: {f1_lr}")
+print(f"Logistic Regression Test Accuracy: {accuracy_lr}")
+print(f"Logistic Regression Test Precision: {precision_lr}")
+print(f"Random Forest F1 Score: {f1_rf}")
+print(f"Random Forest Test Accuracy: {accuracy_rf}")
+print(f"Random Forest Test Precision: {precision_rf}")
+print(f"Support Vector Machine F1 Score: {f1_svm}")
+print(f"Support Vector Machine Accuracy: {accuracy_svm}")
+print(f"Support Vector Machine Precision: {precision_svm}")
+
+#Confusion Matricies
+confusion_matrix_lr = confusion_matrix(y_test, Y_pred_lr)
+confusion_matrix_rf = confusion_matrix(y_test, Y_pred_rf)
+confusion_matrix_svm = confusion_matrix(y_test, Y_pred_svm)
+
+def plot_confusion_matrix(matrix, title):
+    plt.figure(figsize=(6,4))
+    sns.heatmap(matrix, annot=True, fmt="d", cmap='Blues', cbar=False)
+    plt.title(title)
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.show()
+
+plot_confusion_matrix(confusion_matrix_lr, 'Logistic Regression Confusion Matrix')
+plot_confusion_matrix(confusion_matrix_rf, 'Random Forest Confusion Matrix')
+plot_confusion_matrix(confusion_matrix_svm, 'Support Vector Machine Confusion Matrix')
 
 
 
